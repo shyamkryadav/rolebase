9ac1c69da437b17282c558bf74bcb9552dc481ce
shyamkryadav <shyamkryadv345@gmail.com>
2023-10-05 12:02:55 +0545

    code init

--- src/app/ovredue-ticket/ovredue-ticket.component.ts
+++ src/app/ovredue-ticket/ovredue-ticket.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ProxcyDataService } from '../services/proxcy-data.service';
-import { filter } from 'rxjs';
-import { DatePipe } from '@angular/common';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-ovredue-ticket',
@@ -9,20 +8,16 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./ovredue-ticket.component.css']
 })
 export class OvredueTicketComponent {
-
   ticketData:any;
   mainTicketData:any;
-  sliderValue: number = 25; // Initial value
+  sliderValue: number = 25;
   tickteDate :any;
   currentDate = new Date();
-
-   ticketDate: any;
+  ticketDate: any;
   remainingDays: number | undefined;
-
-
+  dueDateDay: any;
   constructor(private _ProxcyDataService:ProxcyDataService){
   }
-
   ngOnInit() {
     this._ProxcyDataService.showAssingData().subscribe((data)=>{
      this.ticketData=data;
@@ -30,32 +25,34 @@ export class OvredueTicketComponent {
     })
 
   }
-//   this.tickteDate= this.mainTicketData.map((item:any)=>item.dueDate)
-//   for(const iterator of this.tickteDate){
-//   const overdueDate = this.mainTicketData.filter((item:any) => new Date(iterator).getDay() < this.sliderValue);
-//   console.log(overdueDate.dueDate)
-//   this.ticketData=overdueDate;
-// }
-
   updateValue(event: any) {
-    const currentDateAsString = this.currentDate;
+    // const currentDateAsString = this.currentDate;
     this.sliderValue = event.target.value;
     // console.log(currentDateAsString.getDay())
     // console.log(this.sliderValue)
 
-    
-    const overdueDate = this.mainTicketData.filter((item:any) => item.dueDate <= this.sliderValue);
+    const overdueDate = this.mainTicketData.filter((item:any) => item.id  <= this.sliderValue);
     console.log(overdueDate.dueDate)
     this.ticketData=overdueDate;
 
-  }
 
+    // const overdueDate = this.mainTicketData;
+
+    // for (const item of overdueDate) {
+    //   const dueDateDay= new Date(item.dueDate)
+    //   const dueDateValue =dueDateDay.getDay()
+    // }
+    // const overdueDates = this.dueDateDay.filter((item:any) => item.getDay() <= this.sliderValue);
+    // console.log(overdueDates)
+    // this.ticketData=overdueDates;
+  }
 }
 
 
+
+//   this.tickteDate= this.mainTicketData.map((item:any)=>item.dueDate)
+//   for(const iterator of this.tickteDate){
+//   const overdueDate = this.mainTicketData.filter((item:any) => new Date(iterator). getDay() < this.sliderValue);
+//   console.log(overdueDate.dueDate)
+//   this.ticketData=overdueDate;
 // }
-
-
-// const overdueDate = this.mainTicketData.filter((item:any) => item.id > this.sliderValue);
-//     console.log(overdueDate.dueDate)
-//     this.ticketData=overdueDate;
